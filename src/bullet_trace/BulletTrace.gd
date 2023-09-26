2b1b075d15ba273c378eec40c3888b98f4b3f4ab
jodi / jodlodi <jodlodi090@gmail.com>
2023-09-26 19:09:24 +0200

    expanded documentation

--- src/bullet_trace/BulletTrace.gd
+++ src/bullet_trace/BulletTrace.gd
@@ -4,7 +4,9 @@ extends Line2D
 ## Class for handling bullet trace animations, using the [Line2D] logic. [br]
 ## An object of this class must always have a shader with a [code]lifetime[/code] and a [code]life[/code] variable.
 
-## Value dictating how long a bullet trace lingers before vanishing.
+## Value dictating how long a bullet trace lingers before vanishing. 
+## The value represents by how much the length of the trace gets divided by. [br]
+## So a trace that is 1000.0 units long, will give us the lifetime of 0.5 seconds, after being divided by 2000.0.
 const TRACE_LIFETIME_FACTOR: float = 2000.0
 
 ## Value dictating how long the bullet trace lingers.
