33cb9fb9649660d5746f9f6b9c9d23d5afdf8447
jodi / jodlodi <jodlodi090@gmail.com>
2023-10-03 17:46:36 +0200

    Added some documentation.

--- src/autoloads/EnemyUtil.gd
+++ src/autoloads/EnemyUtil.gd
@@ -1,9 +1,13 @@
 extends Node
 
+## Array that stores every [Node2D] that is currently aggrod on the [Player]
 var aggro_array : Array[Node2D]
 
+## Function that should be used whenever a [Node2D] aggros onto the [Player] character, so that
+## we can keep track of it.
 func begin_aggro(aggresor: Node2D):
 	aggro_array.append(aggresor)
 
+## Function that tells the game that the provided [Node2D] is no longer aggrod onto the [Player].
 func end_aggro(aggresor: Node2D):
 	aggro_array.erase(aggresor)
