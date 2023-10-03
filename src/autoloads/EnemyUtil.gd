106e74de8cacda79fb78530cf2c6bfc71a4b4735
jodi / jodlodi <jodlodi090@gmail.com>
2023-10-03 20:16:32 +0200

    Switched out an array for the sake of a Dictionary

--- src/autoloads/EnemyUtil.gd
+++ src/autoloads/EnemyUtil.gd
@@ -1,13 +1,13 @@
 extends Node
 
-## Array that stores every [Node2D] that is currently aggrod on the [Player]
-var aggro_array : Array[Node2D]
+## A dictionary that stores every [Node2D] that is currently aggrod on the [Player]
+var aggro_set : Dictionary
 
 ## Function that should be used whenever a [Node2D] aggros onto the [Player] character, so that
 ## we can keep track of it.
 func begin_aggro(aggresor: Node2D):
-	aggro_array.append(aggresor)
+	aggro_set[aggresor] = null
 
 ## Function that tells the game that the provided [Node2D] is no longer aggrod onto the [Player].
 func end_aggro(aggresor: Node2D):
-	aggro_array.erase(aggresor)
+	aggro_set.erase(aggresor)
