860b3a04f79be8cde09fca16c54eb374c174552f
jodi / jodlodi <jodlodi090@gmail.com>
2023-10-04 02:01:42 +0200

    implement suffixes to some export variables :)

--- src/heat/AmbientHeat.gd
+++ src/heat/AmbientHeat.gd
@@ -6,13 +6,14 @@ extends Area2D
 ## The internal temperature this source can heat a target to (In degrees Celsius). [br]
 ## 37.0 is a very comfortable / average body temperature. [br]
 ## A person passes out at 31.0 and is usually dead by the time they hit 24.0.
-@export_range(0.0, 37.0, 0.005, "or_greater") var target_heat: float = 24.0
+@export_range(0.0, 37.0, 0.005, "or_greater", "suffix:°C") var target_heat: float = 24.0
 
 ## The rate at which the target should change temperature. [br]
 ## This value gets multiplied with the current difference in temperature. [br]
 ## A value of 1.0 would mean that the target's internal temperature would hit the target 
-## temperature in 1 second. Do the math.
-@export_range(0.0, 0.1, 0.00001, "or_greater") var rate: float = 0.001
+## temperature in 1 second, while a value of 0.1 would mean it would take 10 seconds. [br]
+## Do the math.
+@export_range(0.0, 0.1, 0.00001, "or_greater", "suffix:/s") var rate: float = 0.001
 
 func _process(delta: float) -> void:
 	for body in get_overlapping_bodies():
