bb3ee68d6ac04eba942193534636f1f2ca5391a4
jodi / jodlodi <jodlodi090@gmail.com>
2023-10-09 22:15:01 +0200

    Fixed random bullshit

--- src/player/PlayerEquipment.gd
+++ src/player/PlayerEquipment.gd
@@ -5,6 +5,8 @@ extends Control
 
 ## Constant ID for the selected item.
 const ID_SELECTION: String = "SELECTION"
+const SELECTION_COLOR: Color = Color(1.0, 1.0, 1.0, 0.0);
+const SELECTION_OUTLINE: Color = Color(1.0, 1.0, 1.0, 1.0);
 
 ## Emitted when the player has chosen to equip something.
 ## Item is either the item stack that the player wishes to equip, or null if the player
@@ -34,31 +36,15 @@ func _ready() -> void:
 func get_inventory() -> Inventory:
 	return _inventory;
 
-
-func _on_inventory_inventory_event(item_stack, cursur_position, ev) -> void:
+func _on_inventory_inventory_event(item_stack: InventoryItemStack, cursur_position: Vector2i, ev: InputEvent) -> void:
 	if ev is InputEventMouseButton && ev.pressed && ev.button_index == MOUSE_BUTTON_LEFT:
-		var hovered_rect: Rect2i;
-		
 		if item_stack == null:
-			hovered_rect = Rect2i(cursur_position, Vector2i.ONE);
-		else:
-			hovered_rect = Rect2i(item_stack.position, item_stack.get_rotated_size());
-		
-		var selection: InventoryRenderer.ItemSelection = _inventory_renderer.get_selection(ID_SELECTION)
-		if selection != null and selection.get_rect() == Rect2i(hovered_rect.position, hovered_rect.size):
 			_inventory_renderer.clear_selection(ID_SELECTION);
-		elif item_stack == null:
-			_inventory_renderer.clear_selection(ID_SELECTION);
-		else:
-			var color: Color = get_theme_color("selected_color", "InventoryRenderer");
-			var outline: Color = get_theme_color("selected_outline", "InventoryRenderer");
-			
-			_inventory_renderer.add_selection(ID_SELECTION, hovered_rect, color, outline);
-		
-		if selection != null and selection.get_rect() != InventoryRenderer.ItemSelection.NO_RECT:
-			select(item_stack);
-		else:
 			select(null);
+		else:
+			var hovered_rect: Rect2i = Rect2i(item_stack.position, item_stack.get_rotated_size());
+			_inventory_renderer.add_selection(ID_SELECTION, hovered_rect, SELECTION_COLOR, SELECTION_OUTLINE);
+			select(item_stack);
 
 ## Make this item stack appear selected in the inventory. Highlights it, displays its name
 ## and description.
